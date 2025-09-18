74a6691a5d8978eb169054662de23ca7aca3ab15
MaciejSzymczak <soft@home.pl>
2025-09-18 07:31:55 +0200

    Update 2025.09.17.sql

--- cd/serverInstall/updates/2025.09.17.sql
+++ cd/serverInstall/updates/2025.09.17.sql
@@ -2,5 +2,28 @@
 INSERT INTO system_parameters (name,value) VALUES ('VERSION 2025.09.17', 'INSTALLED');
 commit;
 
+alter table timetable_notes add (SEARCHABLE_TEXT  varchar2(4000));
+   
+CREATE OR REPLACE TRIGGER trg_timetable_notes_biu
+BEFORE INSERT OR UPDATE
+ON timetable_notes
+FOR EACH ROW
+BEGIN
+  :NEW.SEARCHABLE_TEXT :=
+    xxmsz_tools.erasePolishChars(
+      UPPER(
+        SUBSTR(
+          TO_CLOB(NVL(:NEW.notes_before, ''))
+          || NVL(:NEW.notes_after, '')
+          || NVL(:NEW.internal_notes, ''),
+          1,
+          4000
+        )
+      )
+    );
+END;
+/
+
+
 *** Install 
 	https://github.com/MaciejSzymczak/Plansoft.org/blob/master/cd/serverInstall/updates/Packages/planner_utils.sql
